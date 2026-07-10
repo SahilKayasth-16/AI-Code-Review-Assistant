1673cddc1a69e1c50884f49795ba71bebecb2dce
SahilKayasth-16 <sahilkayasth1612@gmail.com>
2026-07-10 13:38:00 +0530

    Design dashboard layout

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-const Dashboard = () => {
-    return (
-        <h1>Code Review Dashboard</h1>
+import Navbar from "../components/dashboard/Navbar";
+import Sidebar from "../components/dashboard/Sidebar";
+import ReviewHistory from "../components/dashboard/ReviewHistory";
+
+import "../styles/Dashboard.css";
+
+const Dashbaord = () => {
+    return(
+        <div className="dashboard">
+            <Sidebar />
+            <div className="dashboard-content">
+                <Navbar />
+                <div className="dashboard-body">
+                    <ReviewHistory />
+                </div>
+            </div>
+        </div>
     );
 };
 
-export default Dashboard;
+export default Dashbaord;
